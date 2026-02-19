0c7d889a2e2ad08a9e4967618eac9182f3dd18ac
jdejones <jdejones1@gmail.com>
2026-02-19 01:50:02 -0600

    Corrected path for loading data.

--- strategies/utils/config.py
+++ strategies/utils/config.py
@@ -20,5 +20,5 @@ class RepositoryPaths:
         root = Path(strategies_root) if strategies_root else Path.cwd()
         configured_shared = os.getenv("STRATEGIES_SHARED_ROOT", r"C:\Users\jdejo")
         shared_root = Path(configured_shared)
-        market_data_root = Path(os.getenv("MARKET_DATA_ROOT", str(shared_root / "market_data")))
+        market_data_root = Path(os.getenv("MARKET_DATA_ROOT", str(shared_root/ "Market_Data_Processing" / "market_data")))
         return cls(strategies_root=root, shared_root=shared_root, market_data_root=market_data_root)
